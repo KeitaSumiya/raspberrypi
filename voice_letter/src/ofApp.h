bda8b548861333580756d425a966e875e9752e47
Keita Sumiya <ksumiya0318@gmail.com>
2017-06-27 00:09:11 +0900

    add voice letter

---
+++ voice_letter/src/ofApp.h
@@ -0,0 +1,43 @@
+#pragma once
+
+#include "ofMain.h"
+#include "ofxGPIO.h"
+
+class ofApp : public ofBaseApp{
+
+	public:
+    void setup();
+    void update();
+    void draw();
+ 
+    void keyPressed(int key);
+    void keyReleased(int key);
+    void mouseMoved(int x, int y );
+    void mouseDragged(int x, int y, int button);
+    void mousePressed(int x, int y, int button);
+    void mouseReleased(int x, int y, int button);
+    void mouseEntered(int x, int y);
+    void mouseExited(int x, int y);
+    void windowResized(int w, int h);
+    void dragEvent(ofDragInfo dragInfo);
+    void gotMessage(ofMessage msg);
+    void audioIn(float * input, int bufferSize, int nChannels);
+    void audioOut(float * output, int bufferSize, int nChannels);
+    
+    static const int SampleRate = 48000;
+    static const int BufferSizeMax = SampleRate * 5;
+
+    ofRtAudioSoundStream soundStream;
+    float buffer[BufferSizeMax];
+    int recPos;
+    int recSize;
+    int playPos;
+    int mode; //0:off, 1:recording, 2:play
+    GPIO gpioRec;
+    GPIO gpioPlay;
+    GPIO gpioBack;
+    string recButton;
+    string playButton;
+    string playButtonPre;
+    string backButton;
+};
